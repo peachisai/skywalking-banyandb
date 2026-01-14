e1657a715202c5770f4af216380717072a5e20a3
peachisai <2581009893@qq.com>
2026-01-14 22:11:04 +0800

    Fix lint suggestions

--- banyand/dquery/topn.go
+++ banyand/dquery/topn.go
@@ -54,12 +54,8 @@ func (t *topNQueryProcessor) Rev(ctx context.Context, message bus.Message) (resp
 	}
 	n := time.Now()
 	now := bus.MessageID(request.TimeRange.Begin.Nanos)
-	if request.GetFieldValueSort() == modelv1.Sort_SORT_UNSPECIFIED {
-		resp = bus.NewMessage(now, common.NewError("unspecified requested sort direction"))
-		return
-	}
-	if request.GetAgg() == modelv1.AggregationFunction_AGGREGATION_FUNCTION_UNSPECIFIED {
-		resp = bus.NewMessage(now, common.NewError("unspecified requested aggregation function"))
+	if err := t.validateRequest(request); err != nil {
+		resp = bus.NewMessage(now, common.NewError(err.Error()))
 		return
 	}
 	if e := t.log.Debug(); e.Enabled() {
@@ -209,4 +205,14 @@ func (s *sortedTopNList) Next() bool {
 
 func (s *sortedTopNList) Val() *comparableTopNItem {
 	return &comparableTopNItem{s.Items[s.index-1]}
+}
+
+func (t *topNQueryProcessor) validateRequest(request *measurev1.TopNRequest) error {
+	if request.GetFieldValueSort() == modelv1.Sort_SORT_UNSPECIFIED {
+		return errors.New("unspecified requested sort direction")
+	}
+	if request.GetAgg() == modelv1.AggregationFunction_AGGREGATION_FUNCTION_UNSPECIFIED {
+		return errors.New("unspecified requested aggregation function")
+	}
+	return nil
 }
